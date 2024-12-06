3004d51665ce98e1d713d8f621d61b5ff46a44c2
smd <smd@asus>
2024-12-06 14:50:17 +0300

    init

---
+++ src/test/java/ie/shelf/shelfie/ShelfieApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.shelf.shelfie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
