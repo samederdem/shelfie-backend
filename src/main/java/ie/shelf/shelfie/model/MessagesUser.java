94185a05d3860d0b682630e76f2d7c6233fe2cac
smd <smd@asus>
2024-12-09 23:07:56 +0300

    send message user fix

--- src/main/java/ie/shelf/shelfie/model/MessagesUser.java
+++ src/main/java/ie/shelf/shelfie/model/MessagesUser.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "messages_user")  // Explicit table name
@@ -25,6 +26,8 @@ public class MessagesUser {
     private User recv;
 
     private String text;
+
+    @CreationTimestamp
     private String time;
 
     // Getters and Setters
